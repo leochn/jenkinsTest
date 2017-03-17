72006fa783ef8ad43909c4ff9b04cdc1d81709b3
leousst <appchn@163.com>
2017-03-18 07:40:10 +0800

    first commit

---
+++ src/main/java/com/example/controller/RestController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RestController {
+	
+	@RequestMapping("/hello") 
+	@ResponseBody
+	public String helloWorld(){
+		return "helloWorld!";
+	}
+	
+	@RequestMapping("/") 
+	@ResponseBody
+	public String helloIndex(){
+		return "<h2>This is index page!</h2>";
+	}
+
+}
